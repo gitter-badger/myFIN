b14790089a5e7041c8e7f16211745ea7b3e3cdeb
de Christo <dudu.christo@gmail.com>
2017-05-31 10:45:42 +0200

    test travis execution order.

--- test/server.js
+++ test/server.js
@@ -9,6 +9,8 @@ var request = require('request');
 describe('GET /', function() {
   it('returns HTTP status 200', function(done) {
     request('http://localhost:4000', function(err, res, body) {
+       console.log('verifying response...');
+       console.log(res == null ? null : res.statusCode);
        if(res != null)
           expect(res.statusCode).to.equal(200);
         done();
